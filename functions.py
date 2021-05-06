d763ff08267bd57800bd2d8b6f2057bd43a563b4
Babak <khavari@kth.se>
2021-05-06 16:44:54 +0200

    Added LPG transport cost

--- functions.py
+++ functions.py
@@ -188,7 +188,7 @@ def index(rasters, weights):
 
     return sum(raster) / sum(weights)
 
-def lpg_transportation_cost(travel_time):
+def Lpg_transportation_cost(travel_time):
     
     """"The cost of transporting LPG. See https://iopscience.iop.org/article/10.1088/1748-9326/6/3/034002/pdf for the formula 
     
