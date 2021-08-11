7acac978411999632a6b949032e21cb160f3e78c
Babak <khavari@kth.se>
2021-08-11 14:31:04 +0200

    Updated cost_benefit

--- onsstove/technology.py
+++ onsstove/technology.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 import re
 import pandas as pd
 import datetime
+from math import exp
 
 
 class Technology():
@@ -27,7 +28,7 @@ class Technology():
         self.PM25 = PM25
 
 
-    def set_default_values(self, start_year, end_year, discount_rate):
+    def set_default_values(start_year, end_year, discount_rate):
         self.discount_rate = discount_rate
         self.start_year = start_year
         self.end_year = end_year
@@ -70,6 +71,38 @@ electricity = Technology(tech_life=5,
                         efficiency = 0.86)
 
 
+def morbidity(tech, discount_rate, sfu, hhsize):
+    if tech.PM25 < 7.298:
+        rr_alri = 1
+    else:
+        rr_alri = 1 + 2.383 * (1 - exp(-0.004 * (tech.PM25 - 7.298) ** 1.193))
+
+    if tech.PM25 < 7.337:
+        rr_copd = 1
+    else:
+        rr_copd = 1 + 22.485 * (1 - exp(-0.001 * (tech.PM25 - 7.337) ** 0.694))
+
+    if tech.PM25 < 7.505:
+        rr_ihd = 1
+    else:
+        rr_ihd = 1 + 2.538 * (1 - exp(-0.081 * (tech.PM25 - 7.505) ** 0.466))
+
+    if tech.PM25 < 7.345:
+        rr_lc = 1
+    else:
+        rr_lc = 1 + 152.496 * (1 - exp(-0.000167 * (tech.PM25 - 7.345) ** 0.76))
+
+    paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
+    paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
+    paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
+    paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
+
+    morb_alri = hhsize * paf_alri * incidence_rate_alri
+    morb_copd = hhsize * paf_copd * incidence_rate_copd
+    morb_ihd = hhsize * paf_ihd * incidence_rate_ihd
+    morb_lc = hhsize * paf_lc * incidence_rate_lc
+
+    
 
 
                
