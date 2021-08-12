611eacc22e621f73a4ca90ede9ea79a6347bb080
Babak <khavari@kth.se>
2021-08-12 09:51:00 +0200

    Update technology.py

--- onsstove/technology.py
+++ onsstove/technology.py
@@ -16,6 +16,8 @@ class Technology():
                  inv_cost = 0, #in USD
                  infra_cost = 0, # cost of additional infrastructure
                  fuel_cost = 0,
+                 time_of_collection = 0,
+                 time_of_cooking = 0,
                  om_costs = 0, #percentage of investement cost
                  efficiency = 0,#ratio
                  pm25 = 0): # 24-h PM2.5 concentration
@@ -26,6 +28,8 @@ class Technology():
         self.infra_cost = infra_cost
         self.om_costs = om_costs
         self.efficiency = efficiency
+        self.time_of_collection = time_of_collection
+        self.time_of_cooking = time_of_cooking
         self.pm25 = pm25
 
 
@@ -82,115 +86,125 @@ electricity = Technology(tech_life=5,
 
 def morbidity(start_year, end_year, tech, discount_rate, sfu, hhsize):
 
-        if tech.pm25 < 7.298:
-            rr_alri = 1
-        else:
-            rr_alri = 1 + 2.383 * (1 - exp(-0.004 * (tech.pm25 - 7.298) ** 1.193))
+    if tech.pm25 < 7.298:
+        rr_alri = 1
+    else:
+        rr_alri = 1 + 2.383 * (1 - exp(-0.004 * (tech.pm25 - 7.298) ** 1.193))
 
-        if tech.pm25 < 7.337:
-            rr_copd = 1
-        else:
-            rr_copd = 1 + 22.485 * (1 - exp(-0.001 * (tech.pm25 - 7.337) ** 0.694))
+    if tech.pm25 < 7.337:
+        rr_copd = 1
+    else:
+        rr_copd = 1 + 22.485 * (1 - exp(-0.001 * (tech.pm25 - 7.337) ** 0.694))
 
-        if tech.pm25 < 7.505:
-            rr_ihd = 1
-        else:
-            rr_ihd = 1 + 2.538 * (1 - exp(-0.081 * (tech.pm25 - 7.505) ** 0.466))
+    if tech.pm25 < 7.505:
+        rr_ihd = 1
+    else:
+        rr_ihd = 1 + 2.538 * (1 - exp(-0.081 * (tech.pm25 - 7.505) ** 0.466))
 
-        if tech.pm25 < 7.345:
-            rr_lc = 1
-        else:
-            rr_lc = 1 + 152.496 * (1 - exp(-0.000167 * (tech.pm25 - 7.345) ** 0.76))
+    if tech.pm25 < 7.345:
+        rr_lc = 1
+    else:
+        rr_lc = 1 + 152.496 * (1 - exp(-0.000167 * (tech.pm25 - 7.345) ** 0.76))
 
-        paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
-        paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
-        paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
-        paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
+    paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
+    paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
+    paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
+    paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
 
-        coi_alri =
-        coi_copd =
-        coi_ihd =
-        coi_lc =
+    coi_alri =
+    coi_copd =
+    coi_ihd =
+    coi_lc =
 
-        morb_alri = hhsize * paf_alri * 0.5
-        morb_copd = hhsize * paf_copd * incidence_rate_copd
-        morb_ihd = hhsize * paf_ihd * incidence_rate_ihd
-        morb_lc = hhsize * paf_lc * incidence_rate_lc
+    morb_alri = hhsize * paf_alri * 0.5
+    morb_copd = hhsize * paf_copd * incidence_rate_copd
+    morb_ihd = hhsize * paf_ihd * incidence_rate_ihd
+    morb_lc = hhsize * paf_lc * incidence_rate_lc
 
-        cl_copd = {1:0.3, 2:0.2, 3:0.17, 4:0.17, 5:0.16}
-        cl_alri = {1: 0.7, 2: 0.1, 3: 0.07, 4: 0.07, 5: 0.06}
-        cl_lc = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
-        cl_ihd = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
+    cl_copd = {1:0.3, 2:0.2, 3:0.17, 4:0.17, 5:0.16}
+    cl_alri = {1: 0.7, 2: 0.1, 3: 0.07, 4: 0.07, 5: 0.06}
+    cl_lc = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
+    cl_ihd = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
 
-        i = 1
-        morb_vector = []
-        while i < 6:
-            morbidity_alri = cl_alri[i]*coi_alri*morb_alri/(1+discount_rate)**(end_year-start_year)
-            morbidity_copd = cl_copd[i] * coi_copd * morb_copd / (1 + discount_rate) ** (end_year - start_year)
-            morbidity_lc = cl_lc[i] * coi_lc * morb_lc / (1 + discount_rate) ** (end_year - start_year)
-            morbidity_ihd = cl_ihd[i] * coi_ihd * morb_ihd / (1 + discount_rate) ** (end_year - start_year)
+    i = 1
+    morb_vector = []
+    while i < 6:
+        morbidity_alri = cl_alri[i]*coi_alri*morb_alri/(1+discount_rate)**(end_year-start_year)
+        morbidity_copd = cl_copd[i] * coi_copd * morb_copd / (1 + discount_rate) ** (end_year - start_year)
+        morbidity_lc = cl_lc[i] * coi_lc * morb_lc / (1 + discount_rate) ** (end_year - start_year)
+        morbidity_ihd = cl_ihd[i] * coi_ihd * morb_ihd / (1 + discount_rate) ** (end_year - start_year)
 
-            morb_total = morbidity_alri + morbidity_copd + morbidity_lc + morbidity_ihd
+        morb_total = morbidity_alri + morbidity_copd + morbidity_lc + morbidity_ihd
 
-            morb_vector.append(morb_total)
+        morb_vector.append(morb_total)
 
-        morbidity = np.sum(morb_vector)
+    morbidity = np.sum(morb_vector)
 
-        return morbidity
+    return morbidity
 
 
 def mortality(start_year, end_year, tech, discount_rate, sfu, hhsize, vsl):
 
-        if tech.pm25 < 7.298:
-            rr_alri = 1
-        else:
-            rr_alri = 1 + 2.383 * (1 - exp(-0.004 * (tech.pm25 - 7.298) ** 1.193))
+    if tech.pm25 < 7.298:
+        rr_alri = 1
+    else:
+        rr_alri = 1 + 2.383 * (1 - exp(-0.004 * (tech.pm25 - 7.298) ** 1.193))
 
-        if tech.pm25 < 7.337:
-            rr_copd = 1
-        else:
-            rr_copd = 1 + 22.485 * (1 - exp(-0.001 * (tech.pm25 - 7.337) ** 0.694))
+    if tech.pm25 < 7.337:
+        rr_copd = 1
+    else:
+        rr_copd = 1 + 22.485 * (1 - exp(-0.001 * (tech.pm25 - 7.337) ** 0.694))
 
-        if tech.pm25 < 7.505:
-            rr_ihd = 1
-        else:
-            rr_ihd = 1 + 2.538 * (1 - exp(-0.081 * (tech.pm25 - 7.505) ** 0.466))
+    if tech.pm25 < 7.505:
+        rr_ihd = 1
+    else:
+        rr_ihd = 1 + 2.538 * (1 - exp(-0.081 * (tech.pm25 - 7.505) ** 0.466))
 
-        if tech.pm25 < 7.345:
-            rr_lc = 1
-        else:
-            rr_lc = 1 + 152.496 * (1 - exp(-0.000167 * (tech.pm25 - 7.345) ** 0.76))
+    if tech.pm25 < 7.345:
+        rr_lc = 1
+    else:
+        rr_lc = 1 + 152.496 * (1 - exp(-0.000167 * (tech.pm25 - 7.345) ** 0.76))
 
-        paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
-        paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
-        paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
-        paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
+    paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
+    paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
+    paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
+    paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
 
-        mort_alri = hhsize * paf_alri * mortality_rate_alri
-        mort_copd = hhsize * paf_copd * mortality_rate_copd
-        mort_ihd = hhsize * paf_ihd * mortality_rate_ihd
-        mort_lc = hhsize * paf_lc * mortality_rate_lc
+    mort_alri = hhsize * paf_alri * mortality_rate_alri
+    mort_copd = hhsize * paf_copd * mortality_rate_copd
+    mort_ihd = hhsize * paf_ihd * mortality_rate_ihd
+    mort_lc = hhsize * paf_lc * mortality_rate_lc
 
-        cl_copd = {1: 0.3, 2: 0.2, 3: 0.17, 4: 0.17, 5: 0.16}
-        cl_alri = {1: 0.7, 2: 0.1, 3: 0.07, 4: 0.07, 5: 0.06}
-        cl_lc = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
-        cl_ihd = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
+    cl_copd = {1: 0.3, 2: 0.2, 3: 0.17, 4: 0.17, 5: 0.16}
+    cl_alri = {1: 0.7, 2: 0.1, 3: 0.07, 4: 0.07, 5: 0.06}
+    cl_lc = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
+    cl_ihd = {1: 0.2, 2: 0.1, 3: 0.24, 4: 0.23, 5: 0.23}
 
-        i = 1
-        mort_vector = []
-        while i < 6:
-            mortality_alri = cl_alri[i] * vsl * mort_alri / (1 + discount_rate) ** (end_year - start_year)
-            mortality_copd = cl_copd[i] * vsl * mort_copd / (1 + discount_rate) ** (end_year - start_year)
-            mortality_lc = cl_lc[i] * vsl * mort_lc / (1 + discount_rate) ** (end_year - start_year)
-            mortality_ihd = cl_ihd[i] * vsl * mort_ihd / (1 + discount_rate) ** (end_year - start_year)
+    i = 1
+    mort_vector = []
+    while i < 6:
+        mortality_alri = cl_alri[i] * vsl * mort_alri / (1 + discount_rate) ** (end_year - start_year)
+        mortality_copd = cl_copd[i] * vsl * mort_copd / (1 + discount_rate) ** (end_year - start_year)
+        mortality_lc = cl_lc[i] * vsl * mort_lc / (1 + discount_rate) ** (end_year - start_year)
+        mortality_ihd = cl_ihd[i] * vsl * mort_ihd / (1 + discount_rate) ** (end_year - start_year)
 
-            mort_total = mortality_alri + mortality_copd + mortality_lc + mortality_ihd
+        mort_total = mortality_alri + mortality_copd + mortality_lc + mortality_ihd
 
-            mort_vector.append(mort_total)
+        mort_vector.append(mort_total)
 
-        mortality = np.sum(mort_vector)
+    mortality = np.sum(mort_vector)
 
-        return mortality
+    return mortality
+
+def time_save(tech):
+
+    if tech.time_of_collection == 0:
+        saved_time =
+
+
+
+
+
 
 
 
