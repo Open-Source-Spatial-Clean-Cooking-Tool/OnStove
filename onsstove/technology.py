aafe534a5267ee70daff4eef2d40a1ba1ff63143
Babak <khavari@kth.se>
2021-09-02 13:57:20 +0200

    created paf
    Reading relative risk and calculating the paf value for each disease

--- onsstove/technology.py
+++ onsstove/technology.py
@@ -64,24 +64,16 @@ class Technology():
         else:
             raise KeyError(idx)
 
-    # def set_default_values(start_year, end_year, discount_rate_social, discount_rate_tech):
-    #     self.discount_rate_social = discount_rate_social
-    #     self.start_year = start_year
-    #     self.end_year = end_year
-    #     self.discount_rate_tech = discount_rate_tech
+    @staticmethod
+    def paf(rr_alri, rr_copd, rr_ihd, rr_lc, sfu):
 
+        paf_alri = (sfu * (rr_alri - 1)) / (sfu * (rr_alri - 1) + 1)
+        paf_copd = (sfu * (rr_copd - 1)) / (sfu * (rr_copd - 1) + 1)
+        paf_ihd = (sfu * (rr_ihd - 1)) / (sfu * (rr_ihd - 1) + 1)
+        paf_lc = (sfu * (rr_lc - 1)) / (sfu * (rr_lc - 1) + 1)
 
-#     start_year = 2020
-#     end_year=2030
-#     discount_rate_tech = 0.08
-#     discount_rate_social = 0.04
-#
-# Technology.set_default_values(start_year = start_year,
-#                               end_year = end_year,
-#                               discount_rate_tech = discount_rate_tech,
-#                               discount_rate_social = discount_rate_social)
-
-
+        return paf_alri, paf_copd, paf_ihd, paf_lc
+        
 def morbidity(start_year, end_year, tech, discount_rate_social, hhsize_R, hhsize_U, coi_alri, coi_lc, coi_copd,
               coi_ihd, inci_alri, inci_lc, inci_copd, inci_ihd, sfu=1):
     """
