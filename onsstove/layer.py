fixed quantiles and units in stats

onsstove/layer.py
@@ -493,12 +493,15 @@ class RasterLayer(Layer):
     def get_quantiles(self, quantiles):
         x = self.layer.flat
         x = x[~np.isnan(x)].copy()
-        return np.quantile(x, quantiles).astype('float32')
+        return np.quantile(x, quantiles)
 
     def quantiles(self, quantiles):
         qs = self.get_quantiles(quantiles)
         layer = self.layer.copy()
         i = 0
+        min_val = np.nanmin(layer)
+        layer = layer - min_val
+        qs = qs - min_val
         while i < (len(qs)):
             if i == 0:
                 layer[(layer >= 0) & (layer <= qs[i])] = quantiles[i] * 100
@@ -537,7 +540,7 @@ class RasterLayer(Layer):
         else:
             layer = self.layer
 
-        layer = layer.astype('float32')
+        #layer = layer.astype('float32')
 
         layer[layer == self.meta['nodata']] = np.nan
 

onsstove/onsstove.py
@@ -1078,7 +1078,7 @@ class OnSSTOVE(DataProcessor):
             df[variable] = df[variable].str.replace('_', ' ')
             df.loc[df[variable] == value, variable] = label
 
-    def points_to_raster(self, dff, variable):
+    def points_to_raster(self, dff, variable, dtype=rasterio.uint8, nodata=111):
         total_bounds = self.mask_layer.layer['geometry'].total_bounds
         height = round((total_bounds[3] - total_bounds[1]) / 1000)
         width = round((total_bounds[2] - total_bounds[0]) / 1000)
@@ -1088,8 +1088,8 @@ class OnSSTOVE(DataProcessor):
             out_shape=(height, width),
             transform=transform,
             all_touched=True,
-            fill=111,
-            dtype=rasterio.uint8)
+            fill=nodata,
+            dtype=dtype)
         meta = dict(driver='GTiff',
                     dtype=rasterized.dtype,
                     count=1,
@@ -1097,7 +1097,7 @@ class OnSSTOVE(DataProcessor):
                     width=width,
                     height=height,
                     transform=transform,
-                    nodata=111,
+                    nodata=nodata,
                     compress='DEFLATE')
         return rasterized, meta, total_bounds
 
@@ -1129,21 +1129,27 @@ class OnSSTOVE(DataProcessor):
                 layer, meta, bounds = self.points_to_raster(dff, 'codes')
         else:
             if metric == 'total':
-                dff['variable'] = dff[variable] * dff['Households']
-                dff = dff.groupby('index')['variable'].sum()
+                dff[variable] = dff[variable] * dff['Households']
+                dff = dff.groupby('index').agg({variable: 'sum', 'geometry': 'first'})
             elif metric == 'per_100k':
-                dff['variable'] = dff[variable] * dff['Households']
-                dff = dff.groupby('index')[['variable', 'Calibrated_pop']].sum()
-                dff['variable'] = dff['variable'] * 100000 / dff['Calibrated_pop']
-                dff = dff['variable']
+                dff[variable] = dff[variable] * dff['Households']
+                dff = dff.groupby('index').agg({variable: 'sum', 'Calibrated_pop': 'sum',
+                                                'geometry': 'first'})
+                dff[variable] = dff[variable] * 100000 / dff['Calibrated_pop']
+            elif metric == 'per_household':
+                dff[variable] = dff[variable] * dff['Households']
+                dff = dff.groupby('index').agg({variable: 'sum', 'Households': 'sum',
+                                                'geometry': 'first'})
+                dff[variable] = dff[variable] / dff['Households']
             else:
-                dff = dff.groupby('index').agg({variable: metric})[variable]
+                dff = dff.groupby('index').agg({variable: metric, 'geometry': 'first'})
             if self.rows is not None:
-                layer[self.rows, self.cols] = dff
+                layer[self.rows, self.cols] = dff[variable]
                 meta = self.base_layer.meta
                 bounds = self.base_layer.bounds
             else:
-                layer, meta, bounds = self.points_to_raster(dff, variable)
+                layer, meta, bounds = self.points_to_raster(dff, variable, dtype='float32',
+                                                            nodata=np.nan)
             variable = variable + '_' + metric
         raster = RasterLayer('Output', variable)
         raster.layer = layer
@@ -1197,13 +1203,13 @@ class OnSSTOVE(DataProcessor):
         texts_vbox = VPacker(children=[deaths, health, emissions, time], pad=0, sep=6)
 
         deaths_avoided = summary.loc['total', 'deaths_avoided']
-        health_costs_avoided = summary.loc['total', 'health_costs_avoided']
-        reduced_emissions = summary.loc['total', 'reduced_emissions']
+        health_costs_avoided = summary.loc['total', 'health_costs_avoided'] / 1000
+        reduced_emissions = summary.loc['total', 'reduced_emissions'] / 1000
         time_saved = summary.loc['total', 'time_saved']
 
         deaths = TextArea(f"{deaths_avoided:.0f} pp/yr", textprops=dict(fontsize=fontsize, color='black'))
-        health = TextArea(f"{health_costs_avoided:.2f} b.USD", textprops=dict(fontsize=fontsize, color='black'))
-        emissions = TextArea(f"{reduced_emissions:.2f} Mton", textprops=dict(fontsize=fontsize, color='black'))
+        health = TextArea(f"{health_costs_avoided:.2f} BUSD", textprops=dict(fontsize=fontsize, color='black'))
+        emissions = TextArea(f"{reduced_emissions:.2f} Bton", textprops=dict(fontsize=fontsize, color='black'))
         time = TextArea(f"{time_saved:.2f} h/pp.day", textprops=dict(fontsize=fontsize, color='black'))
 
         values_vbox = VPacker(children=[deaths, health, emissions, time], pad=0, sep=6, align='right')