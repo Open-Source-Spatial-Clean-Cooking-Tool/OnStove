bbda19732a3bfb3a5ed25f99b743bac41a5f6b4c
Camilo Ramirez <camilorg@kth.se>
2022-11-02 14:39:02 +0100

    model results stat plots test

--- onstove/tests/test_plot_results.py
+++ onstove/tests/test_plot_results.py
@@ -8,7 +8,7 @@ def test_plot_maps():
     results = OnStove.read_model(os.path.join('onstove', 'tests', 'output', 
                                               'results.pkl'))
                                               
-    # 2. Crating result maps
+    # 2. Creating result maps
     cmap = {"Biomass ICS (ND)": '#6F4070', "LPG": '#66C5CC', "Biomass": '#FFB6C1',
             "Biomass ICS (FD)": '#af04b3', "Pellets ICS (FD)": '#ef02f5',
             "Charcoal": '#364135', "Charcoal ICS": '#d4bdc5',
@@ -52,4 +52,44 @@ def test_plot_maps():
                      cumulative_count=[0.01, 0.99], figsize=(7, 5),
                      title=f'Maximum net benefit | {country}', dpi=300,
                      rasterized=True, type='pdf')
+    assert True
+    
+def test_plot_stats():
+    # 1. Reading results
+    country = 'Rwanda'
+    results = OnStove.read_model(os.path.join('onstove', 'tests', 'output', 
+                                              'results.pkl'))
+                                              
+    # 2. Creating result stat plots
+    cmap = {"Biomass ICS (ND)": '#6F4070', "LPG": '#66C5CC', "Biomass": '#FFB6C1',
+            "Biomass ICS (FD)": '#af04b3', "Pellets ICS (FD)": '#ef02f5',
+            "Charcoal": '#364135', "Charcoal ICS": '#d4bdc5',
+            "Biogas": '#73AF48', "Biogas and Biomass ICS (ND)": "#F6029E",
+            "Biogas and Biomass ICS (FD)": "#F6029E",
+            "Biogas and Pellets ICS (FD)": "#F6029E",
+            "Biogas and LPG": "#0F8554", "Biogas and Biomass": "#266AA6",
+            "Biogas and Charcoal": "#3B05DF",
+            "Biogas and Charcoal ICS": "#3B59DF",
+            "Electricity": '#CC503E', "Electricity and Biomass ICS (ND)": "#B497E7",
+            "Electricity and Biomass ICS (FD)": "#B497E7",
+            "Electricity and Pellets ICS (FD)": "#B497E7",
+            "Electricity and LPG": "#E17C05", "Electricity and Biomass": "#FFC107",
+            "Electricity and Charcoal ICS": "#660000",
+            "Electricity and Biogas": "#f97b72",
+            "Electricity and Charcoal": "#FF0000"}
+
+    labels = {"Biogas and Electricity": "Electricity and Biogas",
+              'Collected Traditional Biomass': 'Biomass',
+              'Collected Improved Biomass': 'Biomass ICS (ND)',
+              'Traditional Charcoal': 'Charcoal',
+              'Biomass Forced Draft': 'Biomass ICS (FD)',
+              'Pellets Forced Draft': 'Pellets ICS (FD)'}
+              
+    results.plot_split(cmap=cmap, labels=labels, save_as='tech_split', 
+                       height=1.5, width=3.5)
+    results.plot_costs_benefits(labels=labels, save_as='benefits_costs', 
+                                height=1.5, width=2)
+    results.plot_benefit_distribution(type='box', groupby='None', cmap=cmap, 
+                                      labels=labels, save_as='max_benefits_box',
+                                      height=1.5, width=3.5)
     assert True
