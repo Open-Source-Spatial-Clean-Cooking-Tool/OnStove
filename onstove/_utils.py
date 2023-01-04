edd4905c7dce43a813f7fd7cfeb6f2b3d85f1bde
Camilo Ramirez <camilorg@kth.se>
2023-01-04 16:34:05 +0100

    class fro general processes

--- onstove/_utils.py
+++ onstove/_utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import geopandas as gpd
 from typing import Union, Optional
 
 from onstove.layer import VectorLayer, RasterLayer
@@ -5,7 +7,7 @@ from onstove.layer import VectorLayer, RasterLayer
 
 def raster_setter(layer: Union[RasterLayer, str, None],
                   category: Optional[str] = None,
-                  name: Optional[str] = None):
+                  name: Optional[str] = ''):
     if isinstance(layer, str):
         return RasterLayer(category=category, name=name, path=layer)
     elif isinstance(layer, RasterLayer):
@@ -18,7 +20,7 @@ def raster_setter(layer: Union[RasterLayer, str, None],
 
 def vector_setter(layer: Union[RasterLayer, str, None],
                   category: Optional[str] = None,
-                  name: Optional[str] = None):
+                  name: Optional[str] = ''):
     if isinstance(layer, str):
         return VectorLayer(category=category, name=name, path=layer)
     elif isinstance(layer, VectorLayer):
@@ -27,3 +29,37 @@ def vector_setter(layer: Union[RasterLayer, str, None],
         return None
     else:
         raise ValueError('Vector file type or object not recognized.')
+
+
+class Processes():
+    def __init__(self):
+        self.gdf = gpd.GeoDataFrame()
+
+    def normalize(self, column: str, inverse: bool = False) -> pd.Series:
+        """Uses the MinMax method to normalize the data from a column of the :attr:`gdf` GeoDataFrame.
+
+        Parameters
+        ----------
+        column: str
+            Name of the column of the :attr:`gdf` to create the normalized data from.
+        inverse: bool, default False
+            Whether to invert the range in the normalized data.
+
+        Returns
+        -------
+        pd.Series
+            The normalized pd.Series.
+        """
+        if column in self.gdf.columns:
+            data = self.gdf[column]
+        elif column in self.__dict__.keys():
+            data = self.__dict__[column]
+        else:
+            raise KeyError(f'Variable "{column}" was not found in the data.')
+
+        if inverse:
+            normalized = (data.max() - data) / (data.max() - data.min())
+        else:
+            normalized = (data - data.min()) / (data.max() - data.min())
+
+        return normalized
