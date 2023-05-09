2519f438e57055a6c924457ed74b36e4305117bd
Camilo Ramirez Gomez <camilorg@kth.se>
2023-05-09 13:55:47 +0200

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='onstove',
-    version='0.1.4',
+    version='0.1.3',
     packages=['onstove'],
     python_requires='>=3.10',
     install_requires=['dill',
